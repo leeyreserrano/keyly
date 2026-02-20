8d7cc68c79f9df058790ee23db2580ad8582d3cb
Gerard-Pozo <gerardpozo06@gmail.com>
2026-02-20 12:17:28 +0100

    Taules autogenerades

--- db/mysql-init/init.sql
+++
@@ -1,5 +0,0 @@
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '12345678';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345678';
-ALTER USER 'keyly'@'%' IDENTIFIED WITH mysql_native_password BY '12345678';
-ALTER USER 'keyly'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345678';
-FLUSH PRIVILEGES;
