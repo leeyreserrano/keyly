d7d5e90e795b86dbee3d0702f2ffcb7f4d7d88e6
Gerard-Pozo <gerardpozo06@gmail.com>
2026-03-15 12:47:48 +0100

    Eliminar imports

--- backend/api/src/main/java/com/keyly/exception/GestorGlobalExcepcions.java
+++ backend/api/src/main/java/com/keyly/exception/GestorGlobalExcepcions.java
@@ -2,9 +2,7 @@ package com.keyly.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.validation.ConstraintViolationException;
