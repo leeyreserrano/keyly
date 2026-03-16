f854144a6e70382dc0cb0e4a6ed63f42236f77fa
Gerard-Pozo <gerardpozo06@gmail.com>
2026-03-16 13:00:23 +0100

    ConfigRequest sin redundancia

--- backend/api/src/main/java/com/keyly/model/request/ConfigRequest.java
+++ backend/api/src/main/java/com/keyly/model/request/ConfigRequest.java
@@ -1,13 +1,6 @@
 package com.keyly.model.request;
 
-import java.util.UUID;
-
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
-
 public record ConfigRequest(
-        UUID sucursalUuid,
-        @JsonSetter(nulls = Nulls.AS_EMPTY)
         Boolean permetreTotsDominis) {
 
 }
