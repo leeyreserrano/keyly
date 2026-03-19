687ce1324295f0aef5ba32219cb7d82d744d470b
Gerard-Pozo <gerardpozo06@gmail.com>
2026-03-19 10:32:34 +0100

    Responses arregladas

--- backend/api/src/main/java/com/keyly/controller/AuthController.java
+++ backend/api/src/main/java/com/keyly/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.keyly.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.keyly.model.request.AuthRequest;
+import com.keyly.model.response.LoginResponse;
 import com.keyly.service.AuthService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,13 +20,13 @@ public class AuthController {
     private AuthService service;
 
     @PostMapping("login")
-    public String login(@RequestBody AuthRequest request) {
+    public ResponseEntity<LoginResponse> login(@RequestBody AuthRequest request) {
         boolean faLogin = service.login(request);
 
         if (faLogin)
-            return service.generateToken(request.correu());
+            return ResponseEntity.ok(new LoginResponse(service.generateToken(request.correu())));
 
-        return "FALLASTE";
+        return null;
     }
 
 }
