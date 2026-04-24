232d9942ac7d6ddea9f15a95a11f3019993723a3
Gerard-Pozo <gerardpozo06@gmail.com>
2026-04-24 10:01:55 +0200

    Add new button

--- keyly-extension/src/components/Searcher.tsx
+++ keyly-extension/src/components/Searcher.tsx
@@ -3,8 +3,9 @@ import React, { useState } from "react"
 type SearcherProps = {
   search: string
   setSearch: (value: string) => void
+  onAddClick?: () => void
 }
-function Searcher({ search, setSearch }: SearcherProps) {
+function Searcher({ search, setSearch, onAddClick }: SearcherProps) {
   return (
     <div className="flex gap-3 mb-4">
       <input
@@ -15,7 +16,9 @@ function Searcher({ search, setSearch }: SearcherProps) {
         className="w-full p-2 border border-gray-400 rounded-lg focus:ring-2 focus:ring-purple-200 focus:ring-offset-0 hover:border-gray-600 transition"
       />
 
-      <button className="bg-purple-500 text-white w-40 rounded-lg font-bold text-sm hover:bg-purple-700 transition-colors">
+      <button
+        onClick={onAddClick}
+        className="bg-purple-500 text-white w-40 rounded-lg font-bold text-sm hover:bg-purple-700 transition-colors">
         + Add New
       </button>
     </div>
